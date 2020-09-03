798b23fcff101be8222ff0c75373b701d84ded52
Oscar Barba <racso_181088@hotmail.com>
2020-09-03 16:33:37 -0500

    Added home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Deck from './components/Deck';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import Player from './components/Player';
 
 export default class App extends React.Component {
@@ -14,6 +14,13 @@ export default class App extends React.Component {
           </Route>
           <Route path="/player">
             <Player />
+          </Route>
+          <Route path="/">
+            <div className="start">
+
+              <Link to="/host" className="main-btn" >Anfitrion</Link>
+              <Link to="/player" className="main-btn">Jugador</Link>
+            </div>
           </Route>
         </Switch>
       </BrowserRouter>
