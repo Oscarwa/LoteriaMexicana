b93fe8afd2aa5b1933fa334b6b5702902ed1c6f9
Oscar Barba <racso_181088@hotmail.com>
2020-09-02 20:07:45 -0500

    GH-Page deploy

---
+++ build/precache-manifest.5a701b1034180aa0b19d75de20f03c1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a20e25d6311434e0e095db5fbec215f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4a78bade825e5fc1425",
+    "url": "/static/css/main.c7e962b4.chunk.css"
+  },
+  {
+    "revision": "5b7149a9c6fce65e5978",
+    "url": "/static/js/2.391c2bca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.391c2bca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a78bade825e5fc1425",
+    "url": "/static/js/main.62c14039.chunk.js"
+  },
+  {
+    "revision": "e3212856e927cf38938a",
+    "url": "/static/js/runtime-main.439c2a24.js"
+  }
+]);
