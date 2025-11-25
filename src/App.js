5697863c6f494e65d1db0b8f833249e54f01b9da
NazgulMX <racso_181088@hotmail.com>
2025-11-25 12:32:22 -0600

    router update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React from 'react';
 import './App.css';
 import Deck from './components/Deck';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Player from './components/Player';
 
 export default class App extends React.Component {
   render () {
     return (
       <BrowserRouter>
-        <Switch>
-          <Route path="/host">
-            <Deck />
-          </Route>
-          <Route path="/player">
-            <Player />
-          </Route>
-          <Route path="/">
+        <Routes>
+          <Route path="/host" element={<Deck />} />
+          <Route path="/player" element={<Player />} />
+          <Route path="/" element={
             <div className="start">
-
               <Link to="/host" className="main-btn" >Anfitrion</Link>
               <Link to="/player" className="main-btn">Jugador</Link>
             </div>
-          </Route>
-        </Switch>
+          } />
+        </Routes>
       </BrowserRouter>
     );
   
